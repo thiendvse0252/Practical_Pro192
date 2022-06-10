8cac9fb49c2c73ed95cf9408b3b857d99464eb23
Văn Thiện <82753066+thiendvse0252@users.noreply.github.com>
2022-06-10 11:40:36 +0700

    Update VehicleCollection.java

--- src/se1625/pracrical/controllers/VehicleCollection.java
+++ src/se1625/pracrical/controllers/VehicleCollection.java
@@ -12,16 +12,6 @@ import java.util.List;
 import java.util.Collections;
 
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * This is comment, do not delete 2021.12.30
- * and open the template in the editor.
- */
-/**
- *
- * @author Hoa Doan
- */
 //require using map/......
 public class VehicleCollection extends HashMap<String, Vehicle> implements I_Collection {
 
